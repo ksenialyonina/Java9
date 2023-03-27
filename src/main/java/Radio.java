07867b210714f759463b052d79fbee48fddfcb5a
Ksenia Vi <kseniavi.ly@gmail.com>
2023-03-27 18:09:33 +0300

    Create tests

---
+++ src/main/java/Radio.java
@@ -0,0 +1,67 @@
+public class Radio {
+    private int currentRadioStation;
+    private int currentVolume;
+
+
+    public void nextRadioStation() {
+        if (currentRadioStation < 9) {
+            currentRadioStation = currentRadioStation + 1;
+        } else {
+            currentRadioStation = 9;
+        }
+    }
+
+    public void prevRadioStation() {
+        if (currentRadioStation > 0) {
+            currentRadioStation = currentRadioStation - 1;
+        } else {
+            currentRadioStation = 0;
+        }
+    }
+
+    public void nextVolume() {
+        if (currentVolume < 10) {
+            currentVolume = currentVolume + 1;
+        } else {
+            currentVolume = 10;
+        }
+    }
+
+    public void prevVolume() {
+        if (currentVolume > 0) {
+            currentVolume = currentVolume - 1;
+        } else {
+            currentVolume = 0;
+        }
+    }
+
+    public int getCurrentRadioStation() {
+        return currentRadioStation;
+    }
+
+    public void setCurrentRadioStation(int newCurrentRadioStation) {
+        if (newCurrentRadioStation < 0) {
+            return;
+        }
+        if (newCurrentRadioStation > 9) {
+            return;
+        }
+        currentRadioStation = newCurrentRadioStation;
+    }
+
+    public int getCurrentVolume() {
+        return currentVolume;
+    }
+
+    public void setCurrentVolume(int newCurrentVolume) {
+        if (newCurrentVolume < 0) {
+            return;
+        }
+        if (newCurrentVolume > 10) {
+            return;
+        }
+        currentVolume = newCurrentVolume;
+    }
+
+}
+
